5bee95bc8e88024584f950cb0c3de50413b1dbb8
HidekiLeonardo <leonardohideki778@gmail.com>
2025-01-29 11:06:58 -0300

    feat: Criar as rotas do controller com o mock

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,33 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissoesController {
+
+    // Get = requisição para mostrar as missoes
+    @GetMapping("/listar")
+    public String listarMissao(){
+        return "Missão listadas com sucesso";
+    }
+
+    // Post = requisição para criar as missoes
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missão criada com sucesso";
+    }
+
+    // Put = requisição para alterar as missoes
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missão alterada com sucesso";
+    }
+
+    // Delete = requisição para deletar as missoes
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missão deletada com sucesso";
+    }
+
+}
